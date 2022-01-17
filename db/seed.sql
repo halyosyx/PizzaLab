6af6f5751a1e5e34df2e2d2d97eee09598bcf9fa
Daniel Wajsman <danielwajsman@mac.com>
2022-01-17 14:38:29 -0800

    seed fixes

--- db/seed.sql
+++ db/seed.sql
@@ -1,6 +1,6 @@
 -- Users table seeds here (Example)
 INSERT INTO users (name, email, phone, created_on) 
-VALUES ('Alice', 'alice@gmail.com','412-323-4567', 'now');
+VALUES ('Alice', 'alice@gmail.com','412-323-4567', );
 
 
 
@@ -18,50 +18,46 @@ VALUES ('Large', 320);
 -- Toppings table seeds here (Example)
 INSERT INTO toppings (name, price, icon_url, preview_url)
 VALUES (
-  "Cheese",
+  'Cheese',
   120,
-  "https://i.ibb.co/x2dDJjg/Cheese-thumb.png",
-  "https://i.ibb.co/vw9JQMz/Cheese-Preview.png"
+  'https://i.ibb.co/x2dDJjg/Cheese-thumb.png',
+  'https://i.ibb.co/vw9JQMz/Cheese-Preview.png'
+);
+
+INSERT INTO toppings (name, price, icon_url, preview_url)
+VALUES ('Basil', 120, 'https://i.ibb.co/rdLCvB1/Basil-thumb.png', 'https://i.ibb.co/TRFK0hD/Basil-Preview.png');
+
+
+INSERT INTO toppings (name, price, icon_url, preview_url)
+VALUES (
+  'Mushroom',
+  120,
+  'https://i.ibb.co/wCb31cp/Mushroom-thumb.png',
+  'https://i.ibb.co/HdFrsR3/Mushroom-Preview.png'
 );
 
 INSERT INTO toppings (name, price, icon_url, preview_url)
 VALUES (
-  "Basil",
+  'Pepperoni',
   120,
-  "https://i.ibb.co/rdLCvB1/Basil-thumb.png",
-  "https://i.ibb.co/TRFK0hD/Basil-Preview.png"
-  );
-
-INSERT INTO toppings (name, price, icon_url, preview_url)
-VALUES (
-  "Mushroom",
-  120,
-  "https://i.ibb.co/wCb31cp/Mushroom-thumb.png",
-  "https://i.ibb.co/HdFrsR3/Mushroom-Preview.png"
+  'https://i.ibb.co/9472PW8/Pepperoni-thumb.png',
+  'https://i.ibb.co/fMWFs0S/Pepperoni-Preview.png'
 );
 
 INSERT INTO toppings (name, price, icon_url, preview_url)
 VALUES (
-  "Pepperoni",
+  'Pineapple',
   120,
-  "https://i.ibb.co/9472PW8/Pepperoni-thumb.png",
-  "https://i.ibb.co/fMWFs0S/Pepperoni-Preview.png"
-);
-
-INSERT INTO toppings (name, price, icon_url, preview_url)
-VALUES (
-  "Basil",
-  120,
-  "https://i.ibb.co/LSTcQNG/Pineapple-thumb.png",
-  "https://i.ibb.co/LSTcQNG/Pineapple-thumb.png"
+  'https://i.ibb.co/LSTcQNG/Pineapple-thumb.png',
+  'https://i.ibb.co/LSTcQNG/Pineapple-thumb.png'
 );  
 
 INSERT INTO toppings (name, price, icon_url, preview_url)
 VALUES (
-  "Cheese",
+  'Tomato',
   120,
-  "https://i.ibb.co/b3Rcbnh/Tomato-thumb.png",
-  "https://i.ibb.co/8NHWbQ2/Tomato-Preview.png"
+  'https://i.ibb.co/b3Rcbnh/Tomato-thumb.png',
+  'https://i.ibb.co/8NHWbQ2/Tomato-Preview.png'
 );
 
 
