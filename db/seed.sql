8f2393caba12e3a80570ccf98565672f86f1a1cc
Daniel Wajsman <danielwajsman@mac.com>
2022-01-17 14:21:20 -0800

    db schema and seeds

---
+++ db/seed.sql
@@ -0,0 +1,70 @@
+-- Users table seeds here (Example)
+INSERT INTO users (name, email, phone) 
+VALUES ('Alice', 'alice@gmail.com','412-323-4567');
+
+
+
+-- Pizza Sizes table seeds here (Example)
+INSERT INTO pizza_sizes (name, price) 
+VALUES ('Small', 150);
+
+INSERT INTO pizza_sizes (name, price) 
+VALUES ('Medium', 220);
+
+INSERT INTO pizza_sizes (name, price) 
+VALUES ('Large', 320);
+
+
+-- Toppings table seeds here (Example)
+INSERT INTO toppings (name, price, icon_url, preview_url)
+VALUES (
+  "Cheese",
+  120,
+  "https://i.ibb.co/x2dDJjg/Cheese-thumb.png",
+  "https://i.ibb.co/vw9JQMz/Cheese-Preview.png"
+);
+
+INSERT INTO toppings (name, price, icon_url, preview_url)
+VALUES (
+  "Basil",
+  120,
+  "https://i.ibb.co/rdLCvB1/Basil-thumb.png",
+  "https://i.ibb.co/TRFK0hD/Basil-Preview.png"
+  );
+
+INSERT INTO toppings (name, price, icon_url, preview_url)
+VALUES (
+  "Mushroom",
+  120,
+  "https://i.ibb.co/wCb31cp/Mushroom-thumb.png",
+  "https://i.ibb.co/HdFrsR3/Mushroom-Preview.png"
+);
+
+INSERT INTO toppings (name, price, icon_url, preview_url)
+VALUES (
+  "Pepperoni",
+  120,
+  "https://i.ibb.co/9472PW8/Pepperoni-thumb.png",
+  "https://i.ibb.co/fMWFs0S/Pepperoni-Preview.png"
+);
+
+INSERT INTO toppings (name, price, icon_url, preview_url)
+VALUES (
+  "Basil",
+  120,
+  "https://i.ibb.co/LSTcQNG/Pineapple-thumb.png",
+  "https://i.ibb.co/LSTcQNG/Pineapple-thumb.png"
+);  
+
+INSERT INTO toppings (name, price, icon_url, preview_url)
+VALUES (
+  "Cheese",
+  120,
+  "https://i.ibb.co/b3Rcbnh/Tomato-thumb.png",
+  "https://i.ibb.co/8NHWbQ2/Tomato-Preview.png"
+);
+
+
+
+
+
