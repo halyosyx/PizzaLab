1fae77121f2a8b5b388d07b6ed321c85ba1e0059
Daniel Wajsman <danielwajsman@mac.com>
2022-01-18 18:52:53 -0800

    not sure if it works

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,26 +1,28 @@
 import React from "react";
 import SelectButton from "./SelectButton";
 import './Homepage.scss';
-
+import {Link} from 'react-router-dom';
 
 
 export default function Homepage() {
   return (
     <div className="buttons">
-      
+      <Link to="/custompizza">
       <SelectButton
       primaryText="Create your own!"
       secondaryText="Choose your topings"
       onClick=""
       image="https://i.ibb.co/W3qDgRG/Blank-Canvas.jpg"
-      />
+        />
+      </Link>
+      <Link to="/randompizza">
       <SelectButton
       primaryText="Feeling Lucky?"
       secondaryText="Randomly generate your pizza"
       onClick=""
       image="https://i.ibb.co/pyQh8zr/Feeling-Lucky.jpg"
       />
-      
+      </Link>
     </div>
   )
 }
