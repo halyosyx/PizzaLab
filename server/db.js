bd083def8bd7207a6aa262f2edbe5f7e0ce51456
Daniel Wajsman <danielwajsman@mac.com>
2022-01-20 09:45:14 -0800

    testing custom pizza code

--- server/db.js
+++ server/db.js
@@ -8,3 +8,16 @@ client.connect(function(err) {
 })
 
 module.exports = client;
+
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+});
+
+module.exports = pool;
+
--- src/Components/CustomePage.js
+++ src/Components/CustomePage.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PizzaPreview from './PizzaPreview'
 import ToppingMenu from './ToppingMenu'
+import axios from 'axios'
 
+
+
+export const toppingSelector = React.createContext()
 
 export default function CustomPage() {
+  const [cheese, setCheese] = useState(false)
+
+  function toggleCheese() {
+    console.log("first");
+    setCheese(a => !a)
+  }
+
   return (
       <div>
-        <PizzaPreview />
-        <ToppingMenu />
+        <toppingSelector.Provider value={cheese}>
+          <button onClick={toggleCheese}> Cheese! </button>
+
+          <PizzaPreview />
+          <ToppingMenu />
+        </toppingSelector.Provider>
       </div>
   )
 }
