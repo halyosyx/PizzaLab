895722d66b7ea680d857eddb9457db219ee7bcc9
Christian Aisle Neral <aisle.github@gmail.com>
2022-01-19 20:15:54 -0800

    Implemented routes for toppings and pizza sizes and implemented randomized functionality to the random page

--- src/RandomPage/Buttons.js
+++ src/RandomPage/Buttons.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import "./Buttons.css"
 
 export default function Buttons(props) {
+  const [sizes, setSizes] = useState([]);
+
+  const getSizes = async () => {
+      const response = await fetch("http://localhost:5000/pizzasizes");
+      const jsonData = await response.json();
+
+      setSizes(jsonData);
+  }
+
+  useEffect(() => {
+      getSizes();
+  }, []);
+
   return (
       <h1 className="title">
           SELECT THE SIZE OF YOUR PIZZA
       <section className="sizes">
-          <button className='small_size'>Small Pizza</button>
-          <button className='medium_size'>Medium Pizza</button>
-          <button className='large_size'>Large Pizza</button>
+          {sizes.map(size => (
+            <button key={size.id}>{size.name} Pizza {size.price} </button>
+          ))}
       </section>
       </h1>
   )
