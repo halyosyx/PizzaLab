48859aa5c6200713516f05603dc87207c55a5888
Daniel Wajsman <danielwajsman@mac.com>
2022-01-22 16:46:14 -0800

    hiding items not used in Order Context

--- src/Components/Order/Order.js
+++ src/Components/Order/Order.js
@@ -6,55 +6,55 @@ import { useState, createContext, useEffect } from 'react';
 //v2
 
 
-const getToppings = async () => {
-    const response = await fetch("http://localhost:5000/toppings");
-    const jsonData = await response.json();
-    return jsonData;
-}
+// const getToppings = async () => {
+//     const response = await fetch("http://localhost:5000/toppings");
+//     const jsonData = await response.json();
+//     return jsonData;
+// }
 
 
 export const OrderContext = createContext({});
 
-export const OrderMaker = props => {
+// export const OrderMaker = props => {
 
-  const [toppings, setToppings] = useState([]);
+//   const [toppings, setToppings] = useState([]);
   
-  useEffect( async ()  => {
-    console.log("first");
-    const toppings = await getToppings();
-    setToppings(toppings);
-    console.log(toppings);
-  }, []);
+//   useEffect( async ()  => {
+//     console.log("first");
+//     const toppings = await getToppings();
+//     setToppings(toppings);
+//     console.log(toppings);
+//   }, []);
 
 
 
-  // useEffect( async ()  => {
-  //   const toppings = await getToppings();
-  //   const toppingSelector = await initialOrderCreator(toppings);
-  //   // order = {...toppingSelector}
-  //   setToppings(toppings);
-  //   setOrder(toppingSelector)
-  //   console.log("in useEffect", toppingSelector);
-  // }, []);
+//   // useEffect( async ()  => {
+//   //   const toppings = await getToppings();
+//   //   const toppingSelector = await initialOrderCreator(toppings);
+//   //   // order = {...toppingSelector}
+//   //   setToppings(toppings);
+//   //   setOrder(toppingSelector)
+//   //   console.log("in useEffect", toppingSelector);
+//   // }, []);
 
-  // const [order, setOrder] = useState({});
-
-
-  //set Order items to selector list
-  // function initialOrderCreator(data) {
-  //     let result = {}  
-  //     data.forEach((topping) => {
-  //      result[topping.name] = false;
-  //     })
-  //     return result;
-  // }
+//   // const [order, setOrder] = useState({});
 
 
-  return(
-     <OrderContext.Provider value={{toppings, setToppings}} >
-       {props.children}
-     </OrderContext.Provider>
-  );
-};
+//   //set Order items to selector list
+//   // function initialOrderCreator(data) {
+//   //     let result = {}  
+//   //     data.forEach((topping) => {
+//   //      result[topping.name] = false;
+//   //     })
+//   //     return result;
+//   // }
+
+
+//   return(
+//      <OrderContext.Provider value={{toppings, setToppings}} >
+//        {props.children}
+//      </OrderContext.Provider>
+//   );
+// };
 
 
