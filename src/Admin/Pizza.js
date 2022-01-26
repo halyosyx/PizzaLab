7747f2db4a118b745ef9e8dac181dedec4c9c739
Daniel Wajsman <danielwajsman@mac.com>
2022-01-25 23:32:53 -0800

    pizza update

--- src/Admin/Pizza.js
+++ src/Admin/Pizza.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 export default function Pizza({id}) {
+
+
+const getPizzaToppings = async () => {
+  const myID = id
+  const response = await fetch(`http://localhost:5000/pizza_name/${myID}`);
+  const jsonData = await response.json();
+  return jsonData;
+}
+
+   const [toppings, setToppings] = useState([]);
+
+  useEffect( async () => {
+    const toppings = await getPizzaToppings();
+    setToppings(toppings);
+    console.log(toppings);
+
+  }, []);
+
+
   return (
     <div>
-      Pizza item {id}!
+      <strong>Pizza item {id} | </strong>
+      Toppings selected: {toppings.join(' ')}
     </div>
     )
 }
